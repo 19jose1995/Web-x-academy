63a70703f23705800c0b3a3e3a14f595b6d90840
Jose Ortiz <hortiz1995@gmail.com>
2025-05-15 21:01:03 -0400

    Cambios totales

--- src/App.jsx
+++ src/App.jsx
@@ -61,15 +61,15 @@ function App()
       <section className="py-20 px-6 bg-gradient-to-br from-[#ceeaee] via-[#f8c9dd] to-[#cdd629]" data-aos="fade-up">
   <h2 className="text-4xl font-bold text-center mb-12">Conoce a Nuestros Maestros</h2>
   <div className="grid gap-10 md:grid-cols-3 max-w-6xl mx-auto">
-    {/*<div className="relative bg-white rounded-2xl shadow-xl border-l-8 border-[#f37826] overflow-hidden hover:scale-105 transition">
+    <div className="relative bg-white rounded-2xl shadow-xl border-l-8 border-[#f37826] overflow-hidden hover:scale-105 transition">
       <img src="/maestro1.jpg" alt="María López" className="w-full h-80 object-cover" />
       <div className="absolute inset-0 bg-black bg-opacity-40 flex items-end p-4">
         <div>
-          <h3 className="text-white text-2xl font-semibold">María López</h3>
+          <h3 className="text-white text-2xl font-semibold">-</h3>
           <p className="text-white text-sm">Especialista en Música</p>
         </div>
       </div>
-    </div>*/}
+    </div>
     <div className="relative bg-white rounded-2xl shadow-xl border-l-8 border-[#ec1763] overflow-hidden hover:scale-105 transition">
       <img src="/Maestro3.jpg" alt="Isabel Minguez" className="w-full h-80 object-cover" />
       <div className="absolute inset-0 bg-black bg-opacity-40 flex items-end p-4">
@@ -79,15 +79,15 @@ function App()
         </div>
       </div>
     </div>
-    {/*<div className="relative bg-white rounded-2xl shadow-xl border-l-8 border-[#5568A9] overflow-hidden hover:scale-105 transition">
+    <div className="relative bg-white rounded-2xl shadow-xl border-l-8 border-[#5568A9] overflow-hidden hover:scale-105 transition">
       <img src="/maestro2.jpg" alt="Carlos Ruiz" className="w-full h-80 object-cover" />
       <div className="absolute inset-0 bg-black bg-opacity-40 flex items-end p-4">
         <div>
-          <h3 className="text-white text-2xl font-semibold">Carlos Ruiz</h3>
+          <h3 className="text-white text-2xl font-semibold">-</h3>
           <p className="text-white text-sm">Instructor de Pintura</p>
         </div>
       </div>
-    </div>*/}
+    </div>
   </div>
 </section>
 
